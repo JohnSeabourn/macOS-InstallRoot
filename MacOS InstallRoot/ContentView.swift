32bfa4ec4ac820bc86bb29e8a729b58c9daa560f
John Seabourn <john.p.seabourn@gmail.com>
2020-04-17 19:41:15 -0400

    Initial Commit

---
+++ MacOS InstallRoot/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MacOS InstallRoot
+//
+//  Created by John Seabourn on 4/17/20.
+//  Copyright © 2020 John Seabourn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
